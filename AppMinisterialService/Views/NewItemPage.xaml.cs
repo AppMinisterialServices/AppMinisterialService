28f0c595df9998043ae00965b4f8efc45f38dc9e
wacan <65459249+wacan@users.noreply.github.com>
2020-10-29 16:31:36 -0500

    Agregar archivos de proyecto.

---
+++ AppMinisterialService/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using AppMinisterialService.Models;
+using AppMinisterialService.ViewModels;
+
+namespace AppMinisterialService.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
